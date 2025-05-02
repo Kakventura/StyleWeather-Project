cebf553da431ac32f7a6b23b6630b7e14ed3b819
Karinne Ventura <125403596+Kakventura@users.noreply.github.com>
2025-05-01 23:59:33 -0300

    Criação do Logout

--- StyleWeather/src/App.jsx
+++ StyleWeather/src/App.jsx
@@ -1,34 +1,24 @@
-// Esse arquivo contém o componente principal do aplicativo, que configura o roteamento e envolve o aplicativo com o contexto global. Ele também importa os componentes de layout e as páginas principais do aplicativo.
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
-import { LayoutCadastrado } from './layout/LayoutCadastrado/LayoutCadastrado';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
-import { EditarPerfil, Inicial, Ajuda } from './pages';  
-import { LayoutPadrao } from './layout/LayoutPadrao/LayoutPadrao';
-import { Cadastrar } from './pages/Cadastrar';
-import { Login } from './pages/Login';
+import { LayoutPrincipal } from './layout/LayoutPrincipal/LayoutPrincipal';
+import { Inicial } from './pages/Inicial';
+import { FormularioCadastrar, FormularioLogin } from './componentes';
 
-const App = () => {
+function App() {
   return (
-      <AppProvider>
-        <BrowserRouter>
-            <LayoutCadastrado>
-              <Routes>
-                <Route path="/editar-perfil" element={<EditarPerfil />} />
-                <Route path="/ajuda" element={<Ajuda />} />
-                <Route path="/" element={<Inicial />} />
-              </Routes>
-            </LayoutCadastrado>
-            <LayoutPadrao>
-              <Routes>
-                <Route path="/" element={<Inicial />} />
-                <Route path="/cadastrar" element={<Cadastrar />} />
-                <Route path="/login" element={<Login />} />
-              </Routes>
-            </LayoutPadrao>
-        </BrowserRouter>
-      </AppProvider>
+    <AppProvider>
+      <Router>
+        <LayoutPrincipal>
+          <Routes>
+            <Route path="/" element={<Inicial />} />
+            <Route path="/login" element={<FormularioLogin />} />
+            <Route path="/cadastrar" element={<FormularioCadastrar />} />
+          </Routes>
+        </LayoutPrincipal>
+      </Router>
+    </AppProvider>
   );
-};
+}
 
-export { App };
+export {App};
