2bc98ca00456078d1107fe6961c5483fe6c366d4
lehguanaes <lehguanaes@gmail.com>
2025-05-07 02:39:48 -0300

    Atualizando arquivos

--- StyleWeather/vite.config.js
+++ StyleWeather/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Certifique-se de que o Vite cria os arquivos de produção na pasta 'dist' ou outra que você preferir
+  },
+  base: '/StyleWeather/', // Ou '/styleweather/' caso seu projeto esteja em uma subpasta na produção
+});
